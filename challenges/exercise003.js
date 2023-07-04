3ba3ea3ee674e9699cd05896758d3fb6cda4c87f
CharlesMartinUK <charles_martin@hotmail.co.uk>
2023-07-04 18:15:33 +0100

    Add files via upload
    Did exercise 3 tasks

--- challenges/exercise003.js
+++ challenges/exercise003.js
@@ -1,26 +1,72 @@
 export function getSquares(nums) {
 	if (nums === undefined) throw new Error('nums is required');
 	// Your code here!
+    var r = []
+    
+    for (let c of nums){
+        r.push(c * c)
+    }
+    
+    return r
+    
 }
 
 export function camelCaseWords(words) {
 	if (words === undefined) throw new Error('words is required');
 	// Your code here!
+    
+    var  r = ""
+    
+    for(let i in words ){
+        if(i == 0)
+            r += words[i]
+        else
+            r += words[i][0].toUpperCase() + words[i].slice(1)
+    }
+    
+    return r
 }
+
+console.log( camelCaseWords(["hello","world","stuff"])   )
+
+console.log( camelCaseWords(["hello","world","stuff"])   )
 
 export function getTotalSubjects(people) {
 	if (people === undefined) throw new Error('people is required');
 	// Your code here!
+    let r = 0
+    
+    for(let c of people )
+        r+= c.subjects.length
+    
+    return r
 }
 
 export function checkIngredients(menu, ingredient) {
 	if (menu === undefined) throw new Error('menu is required');
 	if (!ingredient) throw new Error('ingredient is required');
 	// Your code here!
+    
+    for(let c of menu) {
+        
+         if(c.ingredients.indexOf(ingredient) >= 0) return true
+    }
+    
+    return false
+    
 }
 
 export function duplicateNumbers(arr1, arr2) {
 	if (arr1 === undefined) throw new Error('arr1 is required');
 	if (arr2 === undefined) throw new Error('arr2 is required');
 	// Your code here!
+    let dups = []
+    
+    for(var c of arr1) {
+        if(arr2.indexOf(c) >= 0) {
+            if (dups.indexOf(c) == -1) dups.push(c) // only add unique
+        }
+    }
+    
+    return dups.sort()
 }
