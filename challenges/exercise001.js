c2a6b5f33680fb96a77fe1e2353fda4836cdf773
CharlesMartinUK <charles_martin@hotmail.co.uk>
2023-07-04 16:58:14 +0100

    Add files via upload
    Did all other tasks for exercise 1

--- challenges/exercise001.js
+++ challenges/exercise001.js
@@ -24,6 +24,10 @@ export function addVAT(originalPrice, vatRate) {
 		throw new Error('originalPrice is requied');
 	if (vatRate === undefined) throw new Error('vatRate is required');
 	// Add your code here!
+    
+    return parseFloat((originalPrice * (1 + (vatRate / 100) )).toFixed(2))
+    
+    
 }
 
 export function getSalePrice(originalPrice, reduction) {
@@ -31,34 +35,76 @@ export function getSalePrice(originalPrice, reduction) {
 		throw new Error('originalPrice is required');
 	if (reduction === undefined) throw new Error('reduction is required');
 	// Add your code here!
+    
+    return parseFloat((originalPrice - (originalPrice * (reduction/100))).toFixed(2))
 }
 
 export function getMiddleCharacter(str) {
 	if (str === undefined) throw new Error('str is required');
 	// Add your code here!
+    
+    let mi = Math.floor(str.length /2)
+    
+    if(str.length % 2 == 0)
+        return str.substr(mi-1,2)
+    else
+        
+        return str[mi]
+    
+    
 }
 
 export function reverseWord(word) {
 	if (word === undefined) throw new Error('word is required');
 	// Add your code here!
+    return [...word].reverse().join("")
 }
 
 export function reverseAllWords(words) {
 	if (words === undefined) throw new Error('words is required');
 	// Add your code here!
+    let r = []
+    for(let w of words)
+        r.push([...w].reverse().join(""))
+    
+    return r
 }
 
 export function countLinuxUsers(users) {
 	if (users === undefined) throw new Error('users is required');
 	// Add your code here!
+    
+    let count = 0
+    
+    for(let u of users) {
+        if(u.type == "Linux") count++
+    }
+    
+    return count
+    
 }
 
 export function getMeanScore(scores) {
 	if (scores === undefined) throw new Error('scores is required');
 	// Add your code here!
+        
+    let a = 0
+    for(let c of scores) {
+        a += c
+    }
+  
+    return parseFloat((a / scores.length).toFixed(2))
 }
 
 export function simpleFizzBuzz(n) {
 	if (n === undefined) throw new Error('n is required');
 	// Add your code here!
+    
+    if((n % 3 == 0) && (n % 5 == 0)) return "fizzbuzz"
+    
+    if(n % 3 == 0)  return "fizz"
+    
+    if(n % 5 == 0)  return "buzz"
+    
+    return n
 }
